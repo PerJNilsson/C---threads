7d4796d0345c4a0b5026b0834edd21ad5ef89de2
perjnilsson <per90nilsson@gmail.com>
2018-10-04 14:28:23 +0200

    finnished everything else but the posix threads

--- threads.c
+++ threads.c
@@ -26,22 +26,24 @@ int main (int argc, char * argv[]){
     cpx_matrix[ix] = cpx_pointer +2*jx;
   }
   */
-  FILE * fp = fopen("ppm-file.ppm","wb");
+  char file_name[128];
+  strcpy(file_name, argv[3]); strcat(file_name, "-power.ppm");
+  printf("%s\n", file_name);
+  FILE * fp = fopen(file_name,"wb");
   (void) fprintf(fp, "P6\n%d %d\n255\n", size_matrix, size_matrix);
   generate_cpx_numbers(cpx_vector, size_matrix);
-  float complex d; 
+  int check = 0;
   for (size_t ix=0; ix<size_matrix; ++ix){
     for (size_t jx=0; jx<size_matrix; ++jx){
+      float complex d; 
       d = newtons_method(cpx_vector[ix]+cpx_vector[jx]*I, pol_degree);
-      //printf("Roots in (%f + %fi)\n", creal(d), cimag(d));
       static unsigned char color[3];
-      color[0] = cabs((1-creal(d))/2.0) * 256.0;
-      color[1] = cabs((1-cimag(d))/2.0) * 256.0;
-      color[2] = cabs((creal(d)+cimag(d))/2.0) *256.0;
-      //(void) fprintf(fp, "%f %f %f", color[0], color[1], color[2]);
-      (void) fwrite(color, 1, 3, fp);
+      color[0] = cabs((1-creal(d))/2.0) * 250.0;
+      color[1] = cabs((1-cimag(d))/2.0) * 250.0;
+      color[2] = cabs((creal(d)+cimag(d))/2.0) *250.0;
+      (void) fwrite(color, 1, 3, fp);	
+      //printf("ix = %f, jx = %f\n", cpx_vector[ix], cpx_vector[jx]);
     }
-    //(void) fprintf(fp, "\n"); 
   }
   (void)fclose(fp);
   return 0;
@@ -52,10 +54,11 @@ int check_first_input(char * argv[]) {
   int nbr_threads;
   if (input1[0] == '-' && input1[1] == 't'){
     nbr_threads = atoi(input1+2);
-    }
+  }
   else {
     printf("Invalid argument passed for number of threads. Please write on the form -t# -l# #.\n");
     nbr_threads = 0;
+    exit(-1);
   }
   return nbr_threads;
 }
@@ -66,10 +69,11 @@ int check_second_input(char * argv[]) {
   int size_matrix;
   if (input2[0] == '-' && input2[1] == 'l'){
     size_matrix = atoi(input2+2);
-    }
+  }
   else {
     printf("Invalid argument passed for matrix size. Please write on the form -t# -l# #.\n");
     size_matrix = 0;
+    exit(-1);
   }
   return size_matrix;
 }
@@ -81,7 +85,8 @@ int check_third_input(char * argv[]) {
   if (isdigit(input3[0])==0){
     printf("Invalid argument passed for degree of polynomial. Please write on the form -t# -l# #.\n");
     pol_degree = 0;
-    }
+    exit(-1);
+  }
   else {
     pol_degree = atoi(input3);
   }
@@ -90,11 +95,10 @@ int check_third_input(char * argv[]) {
 
 float complex multiplication_complex(float complex complex_nbr, int pol_degree){
   float complex tmp_complex = complex_nbr;
-  //printf("\n\nInside function. Re: %f and Im: %f\n\n", creal(complex_nbr), cimag(complex_nbr));
   for (int ix=2; ix <=pol_degree; ++ix){
     complex_nbr = complex_nbr*tmp_complex;
   }
-  if (pol_degree ==1){
+  if (pol_degree ==0){
     complex_nbr = 1;
   }
   return complex_nbr;
@@ -108,54 +112,54 @@ float complex newtons_method(float complex complex_nbr, int pol_degree) {
   float divergence_stop = 10000000000;
   if (pol_degree ==1 ) {
     while(1) {
+      complex_nbr = complex_nbr - complex_nbr/1.0+1.0;
       polynomial = multiplication_complex(complex_nbr, pol_degree);
 
-      complex_nbr = complex_nbr - complex_nbr/1.0+1.0;
-
-      if (cabs(creal(complex_nbr)) > divergence_stop ||  cabs(cimag(complex_nbr)) > divergence_stop){
-	//printf("Not converging(%f,%fi)\n", creal(complex_nbr), cimag(complex_nbr));
-	complex_nbr = 0+0*I;
+      if ((float)cabs(creal(complex_nbr)) > divergence_stop ||  (float)cabs(cimag(complex_nbr)) > divergence_stop){
+        return complex_nbr = 0.0+0.0*I;
 	break;
       }
     
       if ((float)cabs(creal(complex_nbr)) < i && (float)cabs(cimag(complex_nbr)) < i) {
-	//printf("Too close to origin. Re %f, Im %f\n", creal(complex_nbr), cimag(complex_nbr));
-	complex_nbr = 0+0*I;
+	complex_nbr = 0.0+0.0*I;
 	break;
       }
       if ((float) cabs(creal(polynomial))-1.0 < i && (float)cabs(cimag(polynomial)) < i) {
-	//printf("Roots in (%f,%fi)\n\n", creal(complex_nbr), cimag(complex_nbr));
-	break;
+        break;
     }   
-      return complex_nbr;
+      //return complex_nbr;
     }
   }
   
  else {
-   while (1) {
-     polynomial = multiplication_complex(complex_nbr, pol_degree);
+   int cancel_calculation = 0;
+   while (cancel_calculation < 1000000) {
      polynomial_derivative = multiplication_complex(complex_nbr, pol_degree-1);
 
-     complex_nbr = complex_nbr - complex_nbr/(p_g-1.0)+1.0/((p_g-1.0)*polynomial_derivative);
+     complex_nbr = complex_nbr - complex_nbr/p_g+1.0/(p_g*polynomial_derivative);
 
-     if (cabs(creal(complex_nbr)) > divergence_stop ||  cabs(cimag(complex_nbr)) > divergence_stop){
-       //printf("Not converging(%f,%fi)\n", creal(complex_nbr), cimag(complex_nbr));
-       complex_nbr = 0 +0*I;
+     polynomial = multiplication_complex(complex_nbr, pol_degree);
+     
+     // check and breaks if values diverging
+     if ((float)cabs(creal(complex_nbr)) > divergence_stop || (float) cabs(cimag(complex_nbr)) > divergence_stop){
+       complex_nbr = 0.0 + 0.0*I;
        break;
      }
-    
+     // checks and breaks if it's getting to close to origo.
      if ((float)cabs(creal(complex_nbr)) < i && (float)cabs(cimag(complex_nbr)) < i) {
-       //printf("Too close to origin. Re %f, Im %f\n", creal(complex_nbr), cimag(complex_nbr));
-       complex_nbr = 0+0*I;
+       complex_nbr = 0.0 + 0.0*I;
        break;
      }
-     if ((float) cabs(creal(polynomial))-1.0 < i && (float)cabs(cimag(polynomial)) < i) {
-       //printf("Roots in (%f,%fi)\n\n", creal(complex_nbr), cimag(complex_nbr));
+     // if close enough to root, will stop.
+     if ((float) cabs((polynomial-1.0)) < i) {
        break;
-     }   
+        /*&& (float)cabs(cimag(polynomial)) < i*/
+     }
+     //printf("%f and %f\n", creal(complex_nbr), cimag(complex_nbr));
+     cancel_calculation +=1;
    }
-   return complex_nbr;
  }
+  return complex_nbr;
 }
 
 void generate_cpx_numbers(float * allocated_vector, int size_matrix) {
@@ -174,3 +178,8 @@ void generate_cpx_numbers(float * allocated_vector, int size_matrix) {
     }
   }
   }*/
+void generate_poly_roots(float complex * roots, int pol_degree) {
+  for (size_t ix = 0; ix<pol_degree; ++ix){
+    
+  }
+}
